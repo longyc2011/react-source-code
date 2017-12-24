0301e9209c1170bd0e3a103db54dbbe146066e38
Stevens Sun <longyc2011@gmail.com>
2017-12-24 17:11:44 +0800

    初始化11-1 项目

--- src/10-02/src/js/root.js
+++ src/10-02/src/js/root.js
@@ -1,25 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Index from './index';
-import ComponentList from './components/list';
-import ComponentDetails from './components/details';
+
 import {HashRouter as Router, Route, Switch} from 'react-router-dom';
 
 export default class Root extends React.Component {
   render() {
     return (
 
-      <Router>
-        <div>
-          <Route path='/details' component={ComponentDetails}/>
-          <Route path='/list/:id?' component={ComponentList}/>
-          <Route exact path='/' component={Index}/>
-        </div>
-      </Router>
+<div>init2</div>
     );
   };
 }
 
-
 ReactDOM.render(
-  <Root/>, document.getElementById('example'));
+  <Root/>, document.getElementById('mainContainer'));
