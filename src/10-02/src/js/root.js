8e7804ff630b8c3ac9c735a7557d8182516535a7
Stevens Sun <longyc2011@gmail.com>
2017-12-24 17:20:02 +0800

    测试Ant Design react 组件

--- src/10-02/src/js/root.js
+++ src/10-02/src/js/root.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
+import 'antd/dist/antd.css'; 
+import {Button} from 'antd';
 import {HashRouter as Router, Route, Switch} from 'react-router-dom';
 
 export default class Root extends React.Component {
   render() {
     return (
-
-<div>init2</div>
+			<div>
+				<Button type="primary">Primary</Button>
+				<Button>Default</Button>
+				<Button type="ghost">Ghost</Button>
+				<Button type="dashed">Dashed</Button>
+			</div>
     );
   };
 }
